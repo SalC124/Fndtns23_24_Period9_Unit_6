75fec174c8d1d4d08fe175e1413d24dbb3e683d8
SalC124 <Saltcal124@gmail.com>
2024-05-30 23:45:07 -0400

    Plinko Lab, done in 30 minutes (LITERALLY)

--- Lab_Plinko.java
+++ Lab_Plinko.java
@@ -1,14 +1,38 @@
+import java.util.Scanner;
 public class Lab_Plinko {
     public static void dropMarble(int[] slots){
-        // code for step 3 – drop one marble, print path, update slots
-    }
+        int bounce = 0;
+        int totalBounce = 0;
+        for (int i = 0; i < slots.length - 1; i++) {
+            bounce = (int)(Math.random() * 2);
+            if (bounce == 0) {System.out.print("L");} else {System.out.print("R");}
+            totalBounce += bounce;
+            }System.out.println();
+            slots[totalBounce] += 1;
+        }
 
     public static void printBoard(int[] slots){
-        // code for step 4 – print count of marbles in slots
+        for (int i = 0; i < slots.length; i++) {
+            System.out.print(slots[i] + "|");
+        }
     }
 
     public static void main(String[] args) {
-        // most of the program, handles user input
-        // make appropriate calls to dropMarble and printBoard  
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to\t ____  _     ___ _   _ _  _____    _  _\n" + //
+                        "\t\t|  _ \\| |   |_ _| \\ | | |/ / _ \\  | || |\n" + //
+                        "\t\t| |_) | |    | ||  \\| | ' / | | | |_||_|\n" + //
+                        "\t\t|  __/| |___ | || |\\  | . \\ |_| |  _  _\n" + //
+                        "\t\t|_|   |_____|___|_| \\_|_|\\_\\___/  |_||_|\n"); // made using https://www.asciiart.eu/text-to-ascii-art
+
+        System.out.println("How many slots are on the board?");
+        int[] slots = new int[input.nextInt()];
+        System.out.println("How many marbles are you dropping?");
+        int marblesDropped =  input.nextInt();
+
+        for (int i = 0; i < marblesDropped; i++) {
+            dropMarble(slots);
+        }
+        printBoard(slots);
     }
 }
